fec05497fa9dccf9b44764808c6b51072fe9d8d4
zimplexing <zhangx_0728@163.com>
2016-09-20 16:18:25 +0800

    过滤文章内容

--- server/proxy/article.js
+++ server/proxy/article.js
@@ -4,7 +4,7 @@ var Article = function() {};
 //获取文章列表
 Article.getArticleList = function(obj) {
     return new Promise((resolve, reject) => {
-        db_tools.query('article',obj).then(
+        db_tools.query('article',obj,'-content').then(
             function(data) {
                 resolve(data);
             },
