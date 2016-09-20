0c6a5d49ed059d4a2564cc42c412f03772623bd8
flydreame <1302919348@qq.com>
2016-09-20 16:11:28 +0800

    分离header和footer

---
+++ react/components/Footer.jsx
@@ -0,0 +1,10 @@
+//基础库
+import React,{ Component,PropTypes } from 'react';
+
+export default class Footer extends Component{
+    render(){
+        return (
+            <h3>尾部</h3>
+        );
+    };
+}
