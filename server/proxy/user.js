1e4e04730623051208c396a2c62ce901369f88b0
zimplexing <zhangx_0728@163.com>
2016-09-22 11:29:20 +0800

    add interface

--- server/proxy/user.js
+++ server/proxy/user.js
@@ -6,25 +6,26 @@ var User = function() {};
 //用户注册
 User.addUser = function(obj) {
     return new Promise((resolve, reject) => {
-        db_tools.queryByCondition('user', { author: obj.author }).then(function(data) {
+        db_tools.queryByCondition('user', { author: obj.author }).then(data => {
             //用户存在
             if (data.length !== 0) {
                 resolve(statusMsg.registerErr);
                 return;
             }
             db_tools.add('user', obj).then(
-                function(data) {
+                data => {
                     statusMsg.successMsg.data = data.toObject();
-                    if(statusMsg.successMsg.data.password){
+                    if (statusMsg.successMsg.data.password) {
+                        // 不返回用户密码
                         delete statusMsg.successMsg.data.password;
                     }
                     resolve(statusMsg.successMsg);
                 },
-                function(err) {
+                err => {
                     reject(err);
                 }
             );
-        }, function(data) {
+        }, err => {
             reject(err);
         });
 
@@ -35,14 +36,14 @@ User.addUser = function(obj) {
 User.login = function(obj) {
     return new Promise((resolve, reject) => {
         //查询用户是否存在
-        db_tools.queryByCondition('user', { author: obj.author }).then(function(data) {
+        db_tools.queryByCondition('user', { author: obj.author }).then(data => {
             if (data.length === 0) {
                 //返回用户不存在信息
                 resolve(statusMsg.loginNoExistErr);
                 return;
             }
             //查询用户的密码是否错误
-            db_tools.queryByCondition('user', { author: obj.author, password: obj.password }, '-password').then(function(data) {
+            db_tools.queryByCondition('user', { author: obj.author, password: obj.password }, '-password').then(data => {
                 if (data.length === 0) {
                     //返回密码错误信息
                     resolve(statusMsg.loginPwdErr);
@@ -51,12 +52,47 @@ User.login = function(obj) {
                 //返回登录成功
                 statusMsg.successMsg.data = data[0].toObject();
                 resolve(statusMsg.successMsg);
-            }, function(data) {
+            }, err => {
                 reject(err);
             })
-        }, function(data) {
+        }, err => {
             reject(err);
-        })
-    })
+        });
+    });
+};
+
+//修改用户密码
+User.modifyPwd = function(obj) {
+    return new Promise((resolve, reject) => {
+        //判断旧密码是否正确
+        db_tools.queryByCondition('user', { _id: obj.userId }, 'password').then(data => {
+            data = data.toObject();
+            if (data.password !== obj.oldPwd) {
+                reslove(statusMsg.modfiyPwdErr);
+                return;
+            }
+            //修改密码
+            db_tools.edit('user', { _id: obj.userId }).then(data => {
+                //返回成功信息
+                reslove(data);
+            }, err => {
+                reject(data);
+            })
+        }, derrata => {
+            reject(data);
+        });
+    });
+};
+
+//修改用户资料
+User.modfiyUserData = function(obj) {
+    return new Promise((resolve, reject) => {
+        db_tools.edit('user',obj).then(data=>{
+            resolve(data);
+        },err=>{    
+            reject(err);
+        });
+    });
 }
+
 module.exports = User;
