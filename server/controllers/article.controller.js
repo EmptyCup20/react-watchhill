3fbd5df42b80a35cb5afa7f126e63eaa79d7762a
zimplexing <zhangx_0728@163.com>
2016-09-22 15:12:03 +0800

    add some interfaces

--- server/controllers/article.controller.js
+++ server/controllers/article.controller.js
@@ -30,6 +30,7 @@ export function addArticle(req, res, next) {
     var query = req.query,
         temp_dir;
     article.addArticle(query).then(function(data) {
+        //临时文件夹
         temp_dir = fs.statSync(path.resolve('/public/images/temp',data._id));
         //创建临时文件夹
         if(data.code == 0 && !temp_dir) {
@@ -46,20 +47,31 @@ export function modfiyArticle(req, res, next) {
     var query = req.query,
         temp_dir,
         user_dir;
+    /*
+     * @date 2016-9-22
+     * @param src [移动文件的源文件夹]
+     * @param dst [移动文件的目标文件夹]
+     * 遍历源文件夹,将其文件复制到目标文件夹下
+     */
     var moveFile = (src,dst) => {
         fs.readdirSync(src).forEach(file=>{
-            fs.createReadStream(path.resolve(src,file)).pipe(fs.WriteStream(dst));
+            fs.writeFileSync(path.resolve(src,file),fs.readFileSync(dst));
         });
     };
+    
     article.modfiyArticle(query).then(function(data) {
+        //临时文件夹
         temp_dir = fs.statSync(path.resolve('/public/images/temp',data._id));
+        //用户下问文章图片文件夹
         user_dir = fs.statSync(path.resolve('/public/images/',req.session.loginUser.username,'article',data._id))
         // 临时文件夹中的文件,转移到该用户的文件夹下
         if(data.code == 0 && temp_dir) {
             if(user_dir){
                 moveFile(temp_dir,user_dir);
             }else{
-                fs.mkdirSync(user_dir);
+                fs.mkdir(user_dir,(err,stats)=>{
+                    moveFile(temp_dir,user_dir);
+                });
             }
         }
         res.send(data);
