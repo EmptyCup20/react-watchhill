9c8e6317a19294de3c4492f0087b0dddb1e15153
ziyi2 <673191402@qq.com>
2016-10-11 15:05:55 +0800

    文章,用户数据库初始化

--- server/inits/user.init.js
+++ server/inits/user.init.js
@@ -1,67 +1,15 @@
 var user = require('../../mongo/model/user');
 
-
-let initUsers = [
-    {
-        author:'xiangxiao3',
-        password:'1111',
-        email:'xiangxiao3@hikvision.com.cn',
-        tel:'11111111111',
-        team:'Web前端组'
-    },
-    {
-        author:'lisanchuan',
-        password:'1111',
-        email:'lisanchuan@hikvision.com.cn',
-        tel:'11111111111',
-        team:'Web前端组'
-    },
-    {
-        author:'chenguanbin',
-        password:'1111',
-        email:'chenguanbin@hikvision.com.cn',
-        tel:'11111111111',
-        team:'Web前端组'
-    },
-    {
-        author:'zhangbiying',
-        password:'1111',
-        email:'zhangbiying@hikvision.com.cn',
-        tel:'11111111111',
-        team:'Web前端组'
-    },
-    {
-        author:'zouxiumei',
-        password:'1111',
-        email:'zouxiumei@hikvision.com.cn',
-        tel:'11111111111',
-        team:'Web前端组'
-    },
-    {
-        author:'zhangxin14',
-        password:'1111',
-        email:'zhangxin14@hikvision.com.cn',
-        tel:'11111111111',
-        team:'Web前端组'
-    }
-];
-
-
-let query = user.remove();
-
+let query = user.find();
+query.skip(6);
 query.exec((err,docs) => {
-   if(err) {
-       console.log('Clear user model db data failed!');
-   } else {
-       user.create(initUsers,(err) => {
-           if(err) {
-               console.log('Clear user model db data failed!');
-           } else {
-               console.log('Clear user model db data successed!');
-           }
-       });
-   }
+    for(let doc of docs) {
+        doc.remove();
+    }
+    console.log('Init user model db data successed!');
 });
+
+
 
 
 
