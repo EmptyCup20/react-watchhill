8b9f2f59ad84e7556c328ee32018fe4198887c60
flydreame <1302919348@qq.com>
2016-10-14 14:30:49 +0800

    提高文章上限

--- server/inits/article.init.js
+++ server/inits/article.init.js
@@ -1,17 +1,10 @@
 var article = require('../../mongo/model/article');
 
 let query = article.find();
-query.skip(13);
+query.skip(30);
 query.exec((err,docs) => {
   for(let doc of docs) {
       doc.remove();
   }
   console.log('Init article model db data successed!');
 });
-
-
-
-
-
-
-
